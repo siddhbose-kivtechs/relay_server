f6a38dbd5e77da24fb3860ad60e336490002f3d1
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-11-02 05:26:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,19 @@ const openaiVersion = '2023-09-15-preview';
 const openaiKey = process.env.OPENAI_API_KEY;  
 const engine = 'gpt-35-turbo';  
   
+
+  
 const app = express();  
 const PORT = process.env.PORT || 3000;  
 app.use(cors());  
+app.use(express.json());  
   
-async function fetchCompletion(data, headers) {  
+async function fetchCompletion(data) {  
   try {  
     const response = await axios.post(  
       `${openaiUrl}v1/engines/${engine}/completions`,  
       data,  
-      { headers: headers }  
+      { headers: { Authorization: `Bearer ${openaiKey}`, 'OpenAI-Version': openaiVersion } }  
     );  
     return response;  
   } catch (error) {  
@@ -25,27 +28,29 @@ async function fetchCompletion(data, headers) {
   }  
 }  
   
-app.use('*', async (req, res) => {  
-  const headers = {  
-    'Content-Type': 'application/json',  
-    Authorization: `Bearer ${openaiKey}`,  
-    'OpenAI-Version': openaiVersion,  
-  };  
-    
-  const data = {  
-    engine,  
-    prompt: req.body,  
-    max_tokens: 100,  
-    temperature: 1,  
-    top_p: 0.5,  
-    frequency_penalty: 0,  
-    presence_penalty: 0,  
-  };  
+app.all('*', async (req, res) => {  
+  if (req.path === '/v1/chat/completions') {  
+    const data = {  
+      engine,  
+      prompt: req.body.prompt,  
+      max_tokens: 100,  
+      temperature: 1,  
+      top_p: 0.5,  
+      frequency_penalty: 0,  
+      presence_penalty: 0,  
+    };  
   
-  const response = await fetchCompletion(data, headers);  
-  res.send(response.data);  
+    try {  
+      const response = await fetchCompletion(data);  
+      res.json(response.data);  
+    } catch (error) {  
+      res.status(500).json({ error: 'Internal Server Error' });  
+    }  
+  } else {  
+    res.status(404).json({ error: 'Route not found' });  
+  }  
 });  
   
 app.listen(PORT, () => {  
-  console.log(`API is listening on port ${PORT}`);  
+  console.log(`Relay app is listening on port ${PORT}`);  
 });  
