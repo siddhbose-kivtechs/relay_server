7a2ac762a934b5a2e213bf2fc25326827f55e417
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-11-03 12:47:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -31,34 +31,21 @@ app.all('*', async (req, res) => {
   try {  
     const jsonData = req.body;  
     const parsedData = JSON.parse(jsonData);  
-  
-    // if (parsedData && parsedData.messages && Array.isArray(parsedData.messages)) {  
-    //   const messages = parsedData.messages;  
-    //   const prompt = messages.map((message) => message.content).join('\n');  
-    //   console.log(prompt);
-  
-    //   if (prompt && parsedData.model) {  
-    //     const data = {  
-    //       engine: parsedData.model,  
-    //       prompt,  
-    //       max_tokens: 100,  
-    //       temperature: parsedData.temperature || 1,  
-    //       top_p: parsedData.top_p || 0.5,  
-    //       frequency_penalty: parsedData.frequency_penalty || 0,  
-    //       presence_penalty: parsedData.presence_penalty || 0,  
-    //     };  
+
     console.log(parsedData);
   
         // const response = await fetchCompletion(data);  
         // res.json(response);  
         res.json(parsedData);
         return;  
-      }  
+      // }  
     }  
   
     // Block relay to OpenAI if required parameters are not present  
-    res.status(400).json({ error: 'Invalid request' });  
-  } catch (error) {  
+    // res.status(400).json({ error: 'Invalid request' });  
+  // } 
+
+  catch (error) {  
     console.error(error);  
     res.status(500).json({ error: 'Internal Server Error' });  
   }  
