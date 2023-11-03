a9e16b66679c91f6dce502d7d713eb55d836a717
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-11-03 15:58:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,13 +9,12 @@ app.use(cors());
 app.use(express.json());
 
 
-app.all('*', async (req, res) => 
-    {
-       
-          console.log({data:req.body});
-        req.send({data:req.body});
+// ***  ALL METHOD***
+app.all("/", (req, res) => {
+  res.send("<h2>It's Working!</h2>");
 });
-
 app.listen(PORT, () => {
     console.log(`Relay app is listening on port ${PORT}`);
 });
+
+
