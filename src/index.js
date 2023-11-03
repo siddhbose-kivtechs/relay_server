74ebfa36da2777409c300f37336bb11780406b4f
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-11-03 13:00:39 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,57 +1,55 @@
-import express from 'express';  
-import axios from 'axios';  
-import cors from 'cors';  
-  
-const openaiUrl = 'https://genos.openai.azure.com/';  
-const openaiVersion = '2023-09-15-preview';  
-const openaiKey = process.env.OPENAI_API_KEY;  
-const engine = 'gpt-35-turbo';  
-  
-const app = express();  
-const PORT = process.env.PORT || 3000;  
-app.use(cors());  
-app.use(express.json());  
-  
-async function fetchCompletion(data) {  
-  try {  
-    const response = await axios.post(  
-      `${openaiUrl}v1/engines/${engine}/completions`,  
-      data,  
-      { headers: { Authorization: `Bearer ${openaiKey}`, 'OpenAI-Version': openaiVersion } }  
-    );  
-    console.log(response);  
-    return response.data;  
-  } catch (error) {  
-    console.error(error);  
-    throw error;  
-  }  
-}  
-  
-app.all('*', async (req, res) => {  
-  try {  
-    const jsonData = req.body;  
-    // const parsedData = JSON.parse(jsonData);  
+import express from 'express';
+import axios from 'axios';
+import cors from 'cors';
 
-    console.log(parsedData);
-  
-        // const response = await fetchCompletion(data);  
-        // res.json(response);  
-        // res.json(parsedData);
-     res.status(200).json(parsedData);  
-        // return;  
-      // }  
-    }  
-  
-    // Block relay to OpenAI if required parameters are not present  
-    // res.status(400).json({ error: 'Invalid request' });  
-  // } 
+const openaiUrl = 'https://genos.openai.azure.com/';
+const openaiVersion = '2023-09-15-preview';
+const openaiKey = process.env.OPENAI_API_KEY;
+const engine = 'gpt-35-turbo';
 
-  catch (error) {  
-    console.error(error);  
-    res.status(500).json({ error: 'Internal Server Error' });  
-  }  
-});  
-  
-app.listen(PORT, () => {  
-  console.log(`Relay app is listening on port ${PORT}`);  
-});  
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(express.json());
+
+async function fetchCompletion(data) {
+    try {
+        const response = await axios.post(
+            `${openaiUrl}v1/engines/${engine}/completions`,
+            data, { headers: { Authorization: `Bearer ${openaiKey}`, 'OpenAI-Version': openaiVersion } }
+        );
+        console.log(response);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+        throw error;
+    }
+}
+
+app.all('*', async (req, res) => {
+    try {
+        const jsonData = req.body;
+        try {
+            JSON.parse(jsonData);
+            console.log('Valid JSON');
+
+            console.log(jsonData);
+
+            res.status(200).json(jsonData);
+        } 
+        catch (error) {
+            console.log('Invalid JSON');
+              res.status(200).json({data:jsonData});
+        }
+
+
+
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Relay app is listening on port ${PORT}`);
+});
