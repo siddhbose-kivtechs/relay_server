fb90f3eb6465694a9b99d9dcceed8b207eee8257
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-11-03 16:15:37 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -11,14 +11,30 @@ app.use(express.json());
 //  to verify if the data is json or not
 const verify_data = (data) =>
   {
-    return data;
+    // return data;
+     try {  
+    JSON.parse(data);  
+    return true;  
+  } catch (error) {  
+    return false;  
+  }  
 }
 // ***  ALL METHOD***
 
 app.all("*", (req, res) => {
   let check_data=verify_data(req.body);
-  res.send(check_data);
-  console.log(check_data);
+    console.log(req.body);
+    console.log(check_data);
+  if(check_data)
+  {
+    res.send({data:'json data'});
+  }
+  else
+  {
+    res.send({data:'not a json data'});
+  }
+  // res.send(check_data);
+
 });
 
 app.listen(PORT, () => {
