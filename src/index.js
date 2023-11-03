2c3c25968cadecb251821d91d6b6961fd37133ed
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-11-03 15:55:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import express from 'express';
 import axios from 'axios';
 import cors from 'cors';
 
-const openaiUrl = 'https://genos.openai.azure.com/';
-const openaiVersion = '2023-09-15-preview';
-const openaiKey = process.env.OPENAI_API_KEY;
-const engine = 'gpt-35-turbo';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
