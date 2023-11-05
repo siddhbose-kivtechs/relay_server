824d0c2a7b4bf4329b43413616a5386342c4d7cf
siddh-kivtechs <siddhbose@kneoginigmisarch.co.in>
2023-11-06 04:30:01 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,157 +1,127 @@
-import express from 'express';    
-import axios from 'axios';    
-import cors from 'cors';    
-import { createClient } from "@supabase/supabase-js";    
-import { ulid } from 'ulid';    
-import bodyParser from 'body-parser';     
-import helmet from 'helmet';    
-import morgan from 'morgan';    
-import openai from 'openai';      
-    
-    
-const app = express();    
-const PORT = process.env.PORT || 3000;    
-app.use(cors());    
-app.use(bodyParser.json());      
-    
-app.use(morgan('combined'));    
-app.use(helmet());    
-    
-const supabaseUri = process.env.SUPABASE_URI;      
-const supabaseKey = process.env.SUPABASE_KEY;      
-const supabase = createClient(supabaseUri, supabaseKey);    
-    
-const apiKey = process.env.AZURE_KEY;    
-    
-async function invokeOpenAIEndpoint(message) {      
-  const endpoint='https://ginel-gpt.openai.azure.com/openai/deployments/gpt-35-turbo-16k/chat/completions?api-version=2023-07-01-preview';    
-      
-  try {      
-    const response = await axios.post(endpoint, {      
-      prompt: message,      
-      model: 'gpt-35-turbo-16k',      
-      max_tokens: 800,      
-      temperature: 0.7,      
-      top_p: 0.95,      
-      frequency_penalty: 0,      
-      presence_penalty: 0      
-    }, {      
-      headers: {      
-        'Content-Type': 'application/json',      
-        'Authorization': `Bearer ${apiKey}`      
-      }      
-    });      
-      
-    return response.data.choices[0].text.trim();      
-  } catch (error) {      
-    console.error('Error invoking OpenAI endpoint:', error);      
-    throw error;      
-  }      
-}    
-    
-// function isValidFormat(data) {    
-//   if (!data) return false;    
-//   if (!Array.isArray(data.messages)) return false;    
-//   for (let msg of data.messages) {    
-//     if (typeof msg !== 'object' || !msg.role || !msg.content) return false;    
-//   }    
-//   // if (typeof data.stream !== 'boolean') return false;    
-//   if (typeof data.model !== 'string') return false;    
-//   if (typeof data.temperature !== 'number') return false;    
-//   if (typeof data.presence_penalty !== 'number') return false;    
-      
-//   return true;    
-// }    
-function isValidFormat(message) {
-  if (!message.role || !message.content) return false;
-  if (typeof message !== 'object') return false;
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+const { createClient } = require("@supabase/supabase-js");
+const { ulid } = require('ulid');
+const bodyParser = require('body-parser');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const openai = require('openai');
 
-  return true;
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use(morgan('combined'));
+app.use(helmet());
+
+const supabaseUri = process.env.SUPABASE_URI;
+const supabaseKey = process.env.SUPABASE_KEY;
+const supabase = createClient(supabaseUri, supabaseKey);
+
+const apiKey = process.env.AZURE_KEY;
+
+async function invokeOpenAIEndpoint(message) {
+    const endpoint = 'https://ginel-gpt.openai.azure.com/openai/deployments/gpt-35-turbo-16k/chat/completions?api-version=2023-07-01-preview';
+
+    try {
+        const response = await axios.post(endpoint, {
+            prompt: message,
+            model: 'gpt-35-turbo-16k',
+            max_tokens: 800,
+            temperature: 0.7,
+            top_p: 0.95,
+            frequency_penalty: 0,
+            presence_penalty: 0
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${apiKey}`
+            }
+        });
+
+        return response.data.choices[0].text.trim();
+    } catch (error) {
+        console.error('Error invoking OpenAI endpoint:', error);
+        throw error;
+    }
 }
-  
-app.all("*", async(req, res) => {    
-  const data = req.body;    
-  const jsonString = JSON.stringify(data);    
-  const strippedStr = jsonString.replace(/`/g, '');   
-    
-    // check 
- //    const message = req.body.messages[0];
 
- //  if (message instanceof Object)
- //  {
- //      const response = await invokeOpenAIEndpoint(message);     
- //      res.send(response);
- //  }
- // else
- //  {
- //      res.send(' Either no message or not an object');
- //  }
+function isValidFormat(message) {
+    if (!message.role || !message.content) return false;
+    if (typeof message !== 'object') return false;
 
-  if (!data.messages || !Array.isArray(data.messages)) {
-    res.send('No messages found in request body');
-    return;
-  }
+    return true;
+}
 
-  const message = req.body.messages[0];
+app.all("*", async (req, res) => {
+    const data = req.body;
+    const jsonString = JSON.stringify(data);
+    const strippedStr = jsonString.replace(/`/g, '');
 
-  if (message instanceof Object) {
-    const response = await invokeOpenAIEndpoint(message);
-    res.send(response);
-  } else {
-    res.send('Invalid message format');
-  }
-    
-  
-  // if (typeof data === 'object' && data && isValidFormat(data)) {      
-  //   // send the data to OpenAI  
-  //   const response = isValidFormat(strippedStr);  
-  //   res.send(response);  
-  //   console.log('JSON DATA');  
-  // } else {      
-  //   res.json({ type: 'not a valid format or not a json data', data: data });      
-  //   console.log({ type: 'not a valid format or not a json data', data: data });    
-  // }  
-  
-  let log = {  
-    status: "ok",  
-    url: req.originalUrl,  
-    ip_address: req.headers['x-forwarded-for'] || req.connection.remoteAddress,  
-    request_body: req.body,  
-    request_method: req.method,  
-    lat: req.headers['x-vercel-ip-latitude'],  
-    lon: req.headers['x-vercel-ip-longitude'],  
-    city: req.headers['x-vercel-ip-city'],  
-    region: req.headers['x-vercel-ip-country-region'],  
-    country: req.headers['x-vercel-ip-country'],
-    UA: req.headers['user-agent'],
-    // uuid: uuidv4(),
-    date_time: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
-    ulid: ulid()
-  };
+    if (!data.messages || !Array.isArray(data.messages)) {
+        res.send('No messages found in request body');
+        return;
+    }
 
-   // console.log(log);
+    const message = req.body.messages[0];
 
-  // Insert the log entry into Supabase
-  const { data: logEntry, error } = await supabase
-    .from("logs")
-    .insert([log]);
+    if (isValidFormat(message)) {
+        const response = await invokeOpenAIEndpoint(message);
+        res.send(response);
+    } else {
+        res.send('Invalid message format');
+    }
 
-if (error) {  
-  console.error("Error inserting log:", error);  
-  // Handle the error  
-} 
-else {  
-  // Access the inserted data  
-  console.log("Log entry inserted:", logEntry);  
-}  
-    const parsedData = JSON.parse(strippedStr);  
-    console.log(parsedData);  
-});  
-  
+
+    // if (typeof data === 'object' && data && isValidFormat(data)) {      
+    //   // send the data to OpenAI  
+    //   const response = isValidFormat(strippedStr);  
+    //   res.send(response);  
+    //   console.log('JSON DATA');  
+    // } else {      
+    //   res.json({ type: 'not a valid format or not a json data', data: data });      
+    //   console.log({ type: 'not a valid format or not a json data', data: data });    
+    // }  
+
+    let log = {
+        status: "ok",
+        url: req.originalUrl,
+        ip_address: req.headers['x-forwarded-for'] || req.connection.remoteAddress,
+        request_body: req.body,
+        request_method: req.method,
+        lat: req.headers['x-vercel-ip-latitude'],
+        lon: req.headers['x-vercel-ip-longitude'],
+        city: req.headers['x-vercel-ip-city'],
+        region: req.headers['x-vercel-ip-country-region'],
+        country: req.headers['x-vercel-ip-country'],
+        UA: req.headers['user-agent'],
+        // uuid: uuidv4(),
+        date_time: new Date().toLocaleString('en-US', { timeZone: 'Asia/Kolkata' }),
+        ulid: ulid()
+    };
+
+    // console.log(log);
+
+    // Insert the log entry into Supabase
+    const { data: logEntry, error } = await supabase
+        .from("logs")
+        .insert([log]);
+
+    if (error) {
+        console.error("Error inserting log:", error);
+        // Handle the error  
+    } else {
+        // Access the inserted data  
+        console.log("Log entry inserted:", logEntry);
+    }
+    const parsedData = JSON.parse(strippedStr);
+    console.log(parsedData);
+});
+
 
 
 app.listen(PORT, () => {
     console.log(`Relay app is listening on port ${PORT}`);
 });
-
-
